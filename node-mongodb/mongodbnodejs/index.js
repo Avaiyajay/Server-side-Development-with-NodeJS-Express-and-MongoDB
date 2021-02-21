2789d3fa9143b9d163743c28df2456ca0dcbd33d
Jay Avaiya <avaiyajaykumar5555@gmail.com>
2021-02-20 16:36:02 -0800

    mongodb crud operations on collection

--- node-mongodb/mongodbnodejs/index.js
+++ node-mongodb/mongodbnodejs/index.js
@@ -5,20 +5,34 @@ const uri = "mongodb://127.0.0.1:27017/"
 const db = client.connect(uri , (err , client) => {
     if(err) console.log(err);
     else console.log("connected");
-    const db = client.db('Mflix');
-    // db.createCollection("newcollection" , (error , result) => {
+    const db = client.db('test');
+    //     db.createCollection("mycollection" , (error , result) => {
     //     if(error) console.log(error);
     //     else console.log(result.collectionName + "created");
     // })
-    db.collection("newcollection" , (error , result) => {
-        result.insertOne({ "name" : "jay" , "surname" : "avaiya" } , (error) => {
+    // db.collection("newcollection" , (error , result) => {
+    //     result.insertOne({ "name" : "jay" , "surname" : "avaiya" } , (error) => {
+    //         if(error) console.log(error);
+    //         db.collection("newcollection").find({} , (error , result) => {
+    //             if(error) console.log(error);
+    //             result.forEach(element => {
+    //                 console.log(element)
+    //             });
+    //             client.close();
+    //         })
+    //     })
+    // })
+    db.collection("mycollection").find({ "surname" : "avaiya" } , (error , result) => {
+        if(error) console.log(error);
+        result.forEach(element => {
+            console.log(element)
+        });
+        db.collection("mycollection").updateOne({ "surname" : "avaiya" } , { $set : { "surname" : "jay surname" } } , (error , result) => {
             if(error) console.log(error);
-            db.collection("newcollection").find({} , (error , result) => {
+            console.log(result.result.n , "document modified");
+            db.collection("mycollection").drop((error , result) => {
                 if(error) console.log(error);
-                result.forEach(element => {
-                    console.log(element)
-                });
-                client.close();
+                console.log("collection is dropped");
             })
         })
     })
